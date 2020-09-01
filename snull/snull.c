74d251f7127c6698a3ccbae27b9188ddfb377fc9
flipped <greedkiss@163.com>
2020-09-01 12:58:49 +0800

    :tada:rx

--- snull/snull.c
+++ snull/snull.c
@@ -108,6 +108,7 @@ struct snull_packet* snull_get_tx_buffer(struct net_device * dev){
     return pkt;
 }
 
+//往ppool加数据
 void snull_release_buffer(struct snull_packet* pkt){
     unsigned long flags;
     struct snull_priv * priv = netdev_priv(pkt->dev);
@@ -121,12 +122,93 @@ void snull_release_buffer(struct snull_packet* pkt){
         netif_wake_queue(pkt->dev);
 }
 
+//往rx_queue加数据
 void snull_enqueue_buf(struct net_device * dev, struct snull_packet *pkt){
     unsigned long flags;
     struct snull_packet *priv = netdev_priv(dev);
     
     spin_lock_irqsave(&priv->lock, flags);
     pkt->next = priv->rx_queue;
+    priv->rx_queue = pkt;
+    spin_unlock_irqrestore(&priv->lock, flags);
+}
+
+//拿除rx_queue的一个数据
+struct snull_packet* snull_dequeue_buf(struct net_device * dev){
+    struct snull_priv* priv = netdev_priv(dev);
+    struct snull_packet* pkt;
+    unsigned long flags;
+
+    spin_lock_irqsave(&priv->lock, flags);
+    pkt = priv->rx_queue;
+    if(!pkt->next)
+        priv->rx_queue = pkt->next;
+    spin_unlock_irqrestore(&priv->lock, flags);
+    return pkt;
+}
+
+//开启接收中断
+static void snull_rx_ints(struct net_device* dev, int enable){
+    struct snull_priv * priv = netdev_priv(dev);
+    priv->rx_int_enabled = enable;
+}
+
+//复制硬件mac地址,开启queue
+int snull_open(struct net_device * dev){
+    memcpy(dev->dev_addr, "\0SNUL0", ETH_ALEN);
+	if (dev == snull_devs[1])
+		dev->dev_addr[ETH_ALEN-1]++; /* \0SNUL1 */
+	netif_start_queue(dev);
+	return 0;
+}
+
+int snull_release(struct net_device * dev){
+    netif_stop_queue(dev);
+    return 0;
+}
+
+int snull_config(struct net_device *dev, struct ifmap* map){
+    if (dev->flags & IFF_UP) /* can't act on a running interface */
+		return -EBUSY;
+
+	/* Don't allow changing the I/O address */
+	if (map->base_addr != dev->base_addr) {
+		printk(KERN_WARNING "snull: Can't change I/O address\n");
+		return -EOPNOTSUPP;
+	}
+
+	/* Allow changing the IRQ */
+	if (map->irq != dev->irq) {
+		dev->irq = map->irq;
+        	/* request_irq() is delayed to open-time */
+	}
+
+	/* ignore other fields */
+	return 0;
+}
+
+void snull_rx(struct net_device* dev, struct snull_packet * pkt){
+    struct sk_buff * skb;
+    struct snull_priv * priv = netdev_priv(dev);
+
+    skb = dev_alloc_skb(pkt->datalen+2);
+    if(!skb){
+        if (printk_ratelimit())
+			printk(KERN_NOTICE "snull rx: low on mem - packet dropped\n");
+		priv->stats.rx_dropped++;
+		goto out
+    }
+    skb_reverse(skb,2);
+    memcpy(skb_put(skb, pkt->datalen), pkt->data, pkt->datalen);
+
+    skb->dev = dev;
+    skb->protocol = eth_type_trans(skb, dev);
+    skb->ip_summed = CHECKSUM_UNNECESSARY;
+    priv->stats.rx_packets++;
+    priv->stats.rx_bytes+=pkt->datalen;
+    netif_rx(skb);
+out:
+    return ;
 }
 
 
---
+++ snull/snull.h
@@ -0,0 +1,13 @@
+#ifndef __SNULL_H
+#define __SNULL_H
+
+
+#define SNULL_RX_INTR 0x0001
+#define SNULL_TX_INTR 0x0002
+
+#define SNULL_TIMEOUT 5
+
+extern struct net_device * snull_device[];
+
+
+#endif 
